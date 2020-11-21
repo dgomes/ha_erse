1c97967d7b371611e17151cd8d09a22bb89d06fd
Diogo Gomes <dgomes@ua.pt>
2020-11-21 22:19:34 +0000

    fix

--- custom_components/erse/sensor.py
+++ custom_components/erse/sensor.py
@@ -126,7 +126,7 @@ class EletricityEntity(Entity):
     @property
     def unique_id(self):
         return slugify(
-            str(self.operator) + str(self.my_plan) + len(self.utility_meters)
+            str(self.operator) + str(self.my_plan) + str(len(self.utility_meters))
         )
 
     @property
