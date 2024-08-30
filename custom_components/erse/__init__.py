158489ed97c3530a3589de07e0025a597282ac77
Diogo Gomes <dgomes@ua.pt>
2024-08-30 23:42:41 +0100

    fix for 2024.7

--- custom_components/erse/__init__.py
+++ custom_components/erse/__init__.py
@@ -101,10 +101,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data[DOMAIN][entry.entry_id] = operador
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     async def async_simular(service):
         data = {
